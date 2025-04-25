e5dd707a03aa762199fd9b6ef6e133334fa823d9
Tim Jossund <timjossund@gmail.com>
2025-04-25 15:47:06 -0500

    switch to tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
         Real Truth Social Blog
       @endisset
     </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" /> --}}
     <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -19,7 +19,7 @@
     @vite(['resources/css/app.css','resources/css/main.css', 'resources/js/app.js'])
   </head>
   <body>
-    <header class="header-bar mb-3">
+    <header class="header-bar">
       <div class="container d-flex flex-column flex-md-row align-items-center p-3">
         <h4 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-white">Real Truth Social Blog</a></h4>
         @auth
@@ -71,17 +71,17 @@
     </div>
     @endif
     @auth
-    <div class="container flex">
-      <div class="w-1/5 sticky flex flex-col items-center gap-2 pt-6 rounded" style="background-color: #263c58;">
+    <div class="w-full max-w-7xl flex m-auto">
+      <div class="w-1/4 sticky flex flex-col items-center gap-2 pt-6 mt-6 rounded bg-white border border-solid border-gray-400">
         <img style="width: 125px; height: 125px; border-radius: 100px;" class="mb-2" src="{{auth()->user()->avatar}}" />
         <h4 class="text-white text-center mb-4"><strong>Welcome </strong><br> {{auth()->user()->username}}</h4>
         <a class="btn btn-lg w-6/8 bg-white" style="color: #263c58;" href="/profile/{{auth()->user()->username}}">Your Profile</a>
         <a class="btn btn-lg w-6/8 border-t-2 bg-white" style="color: #263c58;" href="/create-post">Create Post</a>
       </div>
-      <div class="w-3/5">
+      <div class="w-2/4">
         {{ $slot }}
       </div>
-      <div class="w-1/5 border-l-2 border-gray-300">
+      <div class="w-1/4 border-l-2 border-gray-300">
 
       </div>
     </div>
@@ -96,8 +96,8 @@
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script>
+    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script> --}}
       $('[data-toggle="tooltip"]').tooltip()
     </script>
   </body>
