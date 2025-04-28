4a1a6c39211211c426e280af3564a275b5046449
Tim Jossund <timjossund@gmail.com>
2025-04-28 14:19:37 -0500

    cleanup

--- resources/views/home-feed.blade.php
+++ resources/views/home-feed.blade.php
@@ -5,7 +5,7 @@
     <div class="flow-root">
       <div role="list" class="flex flex-col">
         @foreach ($posts as $post)
-        <a href="/post/{{$post->id}}" class="bg-white border border-solid border-gray-400 mx-2 my-1 p-4 rounded bg-white">
+        <a href="/post/{{$post->id}}" class="border border-solid border-gray-400 mx-2 my-1 p-4 rounded bg-white">
           <div class="relative flex space-x-3 gap-2">
             <img class="avatar-tiny" src="{{$post->user->avatar}}" />
             <div class="flex min-w-0 flex-1">
@@ -19,7 +19,7 @@
       </div>
     </div>
     {{ $posts->links() }}
-    @else 
+    @else
     <div class="text-center">
       <h2>Hello <strong>{{ auth()->user()->username }}</strong>, your feed is empty.</h2>
       <p class="lead text-muted">Your feed displays the latest posts from the people you follow. If you don&rsquo;t have any friends to follow that&rsquo;s okay; you can use the &ldquo;Search&rdquo; feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <div class="row align-items-center">
       <div class="col-lg-7 py-3 py-md-5">
         <h1 class="display-3">Social Blogging</h1>
-        <p class="lead text-muted">Let us handle the hosting, updates, and the servers. You can just enjoy writing of your favorite topics. This is a place to plog about cultural things and doctrinal things from a Christian point of view. Maybe you just want to follow a creator, that's great! Just creaate an account and you can use the search to read about the topics or follow our creators.</p>
+        <p class="lead text-muted">Let us handle the hosting, updates, and the servers. You can just enjoy writing of your favorite topics. This is a place to blog about cultural things and doctrinal things from a Christian point of view. Maybe you just want to follow a creator, that's great! Just create an account and you can use the search to read about the topics or follow our creators.</p>
       </div>
       <div class="col-lg-5 pl-lg-5 pb-3 py-lg-5">
         <form action="/register" method="POST" id="registration-form">
@@ -13,7 +13,7 @@
             <label for="username-register" class="text-muted mb-1"><small>Username</small></label>
             <input name="username" id="username-register" class="form-control" type="text" placeholder="Pick a username" autocomplete="off" value="{{ old('username') }}" />
             @error('username')
-                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p> 
+                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
             @enderror
           </div>
 
@@ -21,7 +21,7 @@
             <label for="email-register" class="text-muted mb-1"><small>Email</small></label>
             <input name="email" id="email-register" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" value="{{ old('email') }}"/>
             @error('email')
-                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p> 
+                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
             @enderror
           </div>
 
@@ -29,7 +29,7 @@
             <label for="password-register" class="text-muted mb-1"><small>Password</small></label>
             <input name="password" id="password-register" class="form-control" type="password" placeholder="Create a password" />
             @error('password')
-                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p> 
+                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
             @enderror
           </div>
 
@@ -37,7 +37,7 @@
             <label for="password-register-confirm" class="text-muted mb-1"><small>Confirm Password</small></label>
             <input name="password_confirmation" id="password-register-confirm" class="form-control" type="password" placeholder="Confirm password" />
             @error('password_confirmation')
-                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p> 
+                <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
             @enderror
           </div>
 
